7fb197c9b68570604e1d5b78321fb3a52f73859f
Jan Karlsson <jan.karlsson@ericsson.com>
2021-06-07 14:39:18 +0200

    Add test to check that reconnection works with prefer_local

--- snitch_test.py
+++ snitch_test.py
@@ -114,6 +114,46 @@ class TestGossipingPropertyFileSnitch(Tester):
         assert re.search(ipstr.format(NODE1_LISTEN_ADDRESS), out)
         assert re.search(ipstr.format(NODE2_LISTEN_ADDRESS), out)
 
+    def test_prefer_local_reconnect_on_restart(self):
+        """
+        @jira_ticket CASSANDRA-16718
+
+        """
+
+        NODE1_LISTEN_ADDRESS = '127.0.0.1'
+        NODE1_BROADCAST_ADDRESS = '127.0.0.3'
+
+        NODE2_LISTEN_ADDRESS = '127.0.0.2'
+        NODE2_BROADCAST_ADDRESS = '127.0.0.4'
+        NODE2_LISTEN_ADDRESS_DIFFERENT = '127.0.0.5'
+
+        STORAGE_PORT = 7000
+
+        cluster = self.cluster
+        cluster.populate(2)
+        node1, node2 = cluster.nodelist()
+
+        cluster.seeds = [NODE1_BROADCAST_ADDRESS]
+        cluster.set_configuration_options(values={'endpoint_snitch': 'org.apache.cassandra.locator.GossipingPropertyFileSnitch',
+                                                  'listen_on_broadcast_address': 'true'})
+        node1.set_configuration_options(values={'broadcast_address': NODE1_BROADCAST_ADDRESS})
+        node2.auto_bootstrap = True
+        node2.set_configuration_options(values={'broadcast_address': NODE2_BROADCAST_ADDRESS})
+
+        for node in cluster.nodelist():
+            with open(os.path.join(node.get_conf_dir(), 'cassandra-rackdc.properties'), 'w') as snitch_file:
+                snitch_file.write("dc=dc1" + os.linesep)
+                snitch_file.write("rack=rack1" + os.linesep)
+                snitch_file.write("prefer_local=true" + os.linesep)
+
+        node1.start(wait_for_binary_proto=True)
+        node2.start(wait_for_binary_proto=True, wait_other_notice=False)
+
+        node2.stop()
+
+        node2.set_configuration_options(values={'listen_address': NODE2_LISTEN_ADDRESS_DIFFERENT})
+        node2.start(wait_for_binary_proto=True, wait_other_notice=False)
+
 class TestDynamicEndpointSnitch(Tester):
     @pytest.mark.resource_intensive
     @since('3.10')
